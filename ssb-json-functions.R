e1519d019c7a58c096c990a942c2c23e6bb6f002
t_a_ydersbond <t_a_ydersbond@yahoo.no>
2018-03-19 12:13:54 +0100

    Added json functions and mortality table handling

---
+++ ssb-json-functions.R
@@ -0,0 +1,235 @@
+options(encoding="UTF-8")
+
+library(httr)
+# rjstat is used for converting SSB JSON -> Data frame
+library(rjstat)
+# jsonlite is used mainly for converting metadata 
+library(jsonlite)
+#source("ssb-json-functions.R")
+
+
+#
+##########################################################
+########## Fetching data/metadata from SSB ###############
+########################################################## 
+#
+#
+#
+
+# Fetches the meta data for a table, with number tableId, as a JSON structure
+
+getMetaData <- function(tableId) {
+  getUrl <- paste("http://data.ssb.no/api/v0/no/table/",tableId,sep="")
+  d.tmp<-GET(getUrl)
+  content(d.tmp,"text")
+}
+
+# getRawJSONData - Fetches SB-data by POST-request
+# Parameters 
+#  tableId: Number  SB-table
+#  queryData: JSON-formatted query 
+#
+
+getRawJSONData <- function(tableId,queryData) {
+#  POST query request
+   d.tmp <- POST(paste("http://data.ssb.no/api/v0/no/table/",tableId,sep=""), body = queryData, encode = "json", verbose())
+# Returns contents of d.tmp as JSON-formatted text 
+   content(d.tmp, "text")
+}
+
+
+# getJSONData - Fetches SB-data by POST-request. Returns a data table for further processing
+# Parameters 
+#  tableId: Number  SB-table
+#  queryData: JSON-formatted query 
+#  naming: Two relevant values: id - coding and variable names, label - the label-texts
+#
+
+getJSONData <- function(tableId,queryData,naming="id") {
+# Fetches the content processed by fromJSONstat
+   sbtable <- fromJSONstat(getRawJSONData(tableId,queryData),naming=naming)
+# Only dataset is used from sbtable and returned
+   ds <- sbtable[[1]]
+   ds
+}
+
+
+#
+##########################################################
+####### Handling metadata, creating queries  #############
+########################################################## 
+
+
+# From JSON metadata table structure w/no subtable converted to dataframe by jsonlite
+# Adds column "Slct" for selection marking
+# Parameter tableId: Number SB-table
+
+getValuesAndLabels <- function(tableId) {
+    mDF <- fromJSON(getMetaData(tableId))
+    varNms <- mDF[[2]][[1]]  ; varLbls <- mDF[[2]][[1]] ; varNmb <- length(varNms) ;
+    valAndLbl <- list()
+    for (i in 1:varNmb) {
+        xdfi <- data.frame(mDF[[2]][3][[1]][i],  mDF[[2]][4][[1]][i],0)
+        names(xdfi) <- c(varNms[i],paste(varNms[i],"Label",sep=""),"Slct")
+        valAndLbl[[varNms[i]]] <- xdfi
+    }
+    valAndLbl
+}
+
+
+# 
+#  Creates search data frame from marked-up meta data frame metaDF 
+#
+#
+
+createSearchDF <- function(metaDF){
+
+    srchDF <- fromJSON(getQueryData99999())
+    srchDFq  <-  srchDF$query
+    nQueryRows <- length(srchDFq$code) ;
+    
+    nVar <- length(metaDF) ; nSrchVar <- 0 ;
+    
+    for (i in 1:nVar){
+
+        nValues <-  length(metaDF[[i]][,3]) ;
+        allSlct <- metaDF[[i]][1,3] ; nmbSlct <-  sum(metaDF[[i]][,3]) ;
+        topSlct <- metaDF[[i]][nValues,3]
+        
+        if (allSlct==10 || nmbSlct>0) {   # Variable included in search
+            nSrchVar <-  nSrchVar + 1 ;
+            varNm <- names(metaDF[[i]])[1] ;
+            srchDFq$code[nSrchVar] <- varNm ;
+        }
+        if (allSlct==10) {  # All
+            srchDFq$selection[nSrchVar,1] <- "all" ;
+            srchDFq$selection[nSrchVar,2] <- "*" ;
+        }
+        else if (nmbSlct>0&topSlct<2) { # Some values, put them in a list
+            
+            mDi <- metaDF[[i]] ; mDiSlct <- mDi[mDi[,3]==1,1] ;
+            mDiSlct <- paste("\"",mDiSlct,"\"",sep="")
+            srchDFq$selection[nSrchVar,1] <- "item" ;
+            srchDFq$selection[nSrchVar,2] <- paste(mDiSlct,collapse=",") ;
+         }
+        else if (topSlct>1) { # Newest topSlct values
+            srchDFq$selection[nSrchVar,1] <- "top" ;
+            srchDFq$selection[nSrchVar,2] <- as.character(topSlct) ;
+        }
+    }
+
+    srchDF$query <- srchDFq[1:nSrchVar,]
+    # print(length(srchDF$query$code))
+    fjL <- list(format="json-stat")
+    srchDF$response<-fjL
+    
+    srchDF
+     
+}
+
+
+# Patch the generated query by removing the last [] pair
+# This way, we can create a correct query from a data frame (really list/frame hybrid) we generate
+
+queryFromDF <- function(df){
+
+    jfJ0 <- toJSON(df)
+    jfJ0 <- gsub("\\\\","",jfJ0)
+    jfJ0 <- gsub("\\[\"\"","\\[\"",jfJ0)
+    jfJ0 <- gsub("\"\"\\]","\"\\]",jfJ0)
+    jfJ0 <- gsub("\\]([^]]*)$","\\1",jfJ0)
+    jfJ0 <- gsub("\\[([^[]*)$","\\1",jfJ0)
+    jfJ0
+    
+}    
+
+#  createSearchFromDF - Creates  json search query from marked-up metadata 
+#  Parameter: markedValueLabelsDF - list of data frames marked for selection
+
+createSearchFromDF <- function(markedValueLabelsDF){
+
+   sDF <- createSearchDF(markedValueLabelsDF)
+   queryFromDF(sDF)
+
+}
+
+
+
+# Queries for SSB statbank - structure
+# Dummy query for new query generation - just  to have enough variables
+# This gets rewritten
+
+getQueryData99999 <- function() {
+    '{
+  "query": [
+  {
+    "code":"Kjonn",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"AlderX",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"ContentsCode",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+   "code":"ContentsCode0",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+   "code":"ContentsCode1",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+   "code":"ContentsCode2",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+   "code":"ContentsCode3",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"ContentsCode4",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"ContentsCode5",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+   
+    "code":"ContentsCode6",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"ContentsCode7",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"Tid",
+    "selection": {
+    "filter": "all",
+    "values":["*"] }
+   }   ],
+
+  "response": {
+  "format": "json-stat"
+  } 
+}'
+}
+
+
+
---
+++ ssb_mortality_table_testing.R
@@ -0,0 +1,239 @@
+options(encoding="UTF-8")
+
+# source("ssb_mortality_table_testing.R")
+library(httr)
+# rjstat is used for converting SSB JSON -> Data frame
+library(rjstat)
+# jsonlite is used mainly for converting metadata 
+library(jsonlite)
+# Reshape is used for filtering/transforming/grouping 
+library(reshape)
+
+source("ssb-json-functions.R")
+#graphics.off()
+
+
+
+
+# This query will fetch the whole table 07902
+
+getQueryData07902 <- function() {
+'{
+  "query": [
+  {
+    "code":"Kjonn",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"AlderX",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"ContentsCode",
+     "selection": {
+    "filter": "all",
+    "values":["*"] }
+  },{
+    "code":"Tid",
+    "selection": {
+    "filter": "all",
+    "values":["*"] }
+   }   ],
+  "response": {
+  "format": "json-stat"
+  } 
+}'
+}
+
+
+
+
+getMDValuesLabels07902 <- function(){
+
+    getValuesAndLabels("07902")
+    
+}
+
+
+getAllMortalityData07902 <- function(){
+
+    getJSONData("07902",getQueryData07902())
+    
+}
+
+
+#
+#
+#
+
+testMetaDataAndData07902 <- function(){
+    metaData <- getMDValuesLabels07902() ; 
+    tableData <- getAllMortalityData07902()
+    list(metaData=metaData,tableData=tableData)
+}
+
+#
+# Example of "manual" setup for data extraction 
+#
+
+pickMortalityYears <- function(mdVL,yearsPicked=c(47,48,49,50,51)){
+    mdVL$Kjonn[1,3] <- 10 ; # All
+    mdVL$AlderX[1,3] <- 10 ; # All
+    mdVL$ContentsCode[1,3] <- 10 ; # All
+    for (i in 1:length(yearsPicked))  mdVL$Tid[yearsPicked[i],3] <- 1 ; # Selected
+    
+    mdVL
+}
+
+#
+# Fetching expected life time data
+#
+
+getMortalityYearsData <- function(yearsPicked){
+    
+    metaData <- getMDValuesLabels07902() ;     
+    mdVL <- pickMortalityYears(metaData,yearsPicked=yearsPicked) ;
+    eQuery <- createSearchFromDF(mdVL) ;
+    eData <- getJSONData("07902",eQuery)
+  
+    eData
+}
+
+
+#
+#  Using melt&cast from reshape to transform to columns
+#
+
+transformToColumnsYearsData <- function(eData,yearsPicked){
+
+    eData$expLT <- eData$value # To avoid default naming collision on "value"
+    eData$value <- NULL ;  # Drop value column
+
+    if (length(yearsPicked)==1) {
+        eData$Tid <- NULL # Drop trivial column
+        meltYears <- melt(eData,id=c("Kjonn","AlderX","ContentsCode")) ;
+        meltYears$expLT <- NULL ;  # Drop trivial column
+        colExp <- cast(meltYears,Kjonn+AlderX ~ContentsCode)
+    }
+    else {
+        meltYears <- melt(eData,id=c("Kjonn","AlderX","ContentsCode","Tid")) ;
+        meltYears$expLT <- NULL ;
+        colExp <- cast(meltYears,Kjonn+AlderX ~Tid+ContentsCode)
+    }
+    colExp 
+}
+
+
+#
+# Putting it all together - this function will give expected life times in columns
+# Grouped by sex in first column  
+#
+
+getMortalityYearsDataTable <- function(yearsPicked=c(51)){
+
+    eData <- getMortalityYearsData(yearsPicked)
+    transformToColumnsYearsData(eData,yearsPicked)
+
+}
+
+#
+# Example of "manual" setup for data extraction 
+#
+
+pickMortalityVars <- function(mdVL,contCode=3){
+    mdVL$Kjonn[1,3] <- 10 ; # All
+    mdVL$AlderX[1,3] <- 10 ; # All
+    mdVL$ContentsCode[contCode,3] <- 1 ; # Expected life years
+    mdVL$Tid[1,3] <- 10 ; # All
+    
+    mdVL
+}
+
+
+#
+# Fetching expected life time data
+#
+
+getMortalityData <- function(contCode){
+    
+    metaData <- getMDValuesLabels07902() ;     
+    mdVL <- pickMortalityVars(metaData,contCode=contCode) ;
+    eQuery <- createSearchFromDF(mdVL) ;
+    eData <- getJSONData("07902",eQuery)
+    eData$ContentsCode <- NULL # Drop trivial column
+    eData
+}
+
+
+#
+#  Using melt&cast from reshape to transform to columns
+#
+
+transformToColumnsData <- function(eData){
+    eData$expLT <- eData$value # To avoid default naming collision on "value"
+    eData$value <- NULL ;  # Drop value column
+    meltExpectation <- melt(eData,id=c("Kjonn","AlderX","Tid")) ;
+    meltExpectation$expLT <- NULL ;  # Drop trivial column
+    colExp <- cast(meltExpectation,Kjonn+AlderX~Tid)
+
+    colExp 
+}
+
+
+#
+# Putting it all together - this function will give expected life times in columns
+# Grouped by sex in first column  
+#
+
+getMortalityDataTable <- function(contCode){
+
+    eData <- getMortalityData(contCode)
+    transformToColumnsData(eData)
+
+}
+
+
+
+getSurvivalTable <- function(){
+
+    getMortalityDataTable(1)
+  
+}
+
+getDeathsTable <- function(){
+
+    getMortalityDataTable(2)
+  
+}
+
+
+
+getExpectedLifeTimeTable <- function(){
+
+    getMortalityDataTable(3)
+  
+}
+
+
+getProbabilityOfDeathTable <- function(){
+
+    getMortalityDataTable(4)
+  
+}
+
+
+#survTable <- getSurvivalTable()
+
+#numDeaths <- getDeathsTable()
+#eLTTable <- getExpectedLifeTimeTable()
+#probDeath <- getProbabilityOfDeathTable()
+
+#mort2016 <- getMortalityYearsDataTable(yearsPicked=c(51))
+#mort20142016 <- getMortalityYearsDataTable(yearsPicked=c(49,50,51))
+
+
+
+
+
